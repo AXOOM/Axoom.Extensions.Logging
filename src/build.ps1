b214e377febcb928d12fd8cc7c1d64af30474e49
Bastian Eicher <bastian@eicher.net>
2018-03-06 15:53:13 +0100

    Improves build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -1,9 +1,8 @@
 Param ([string]$Version = "0.1-debug")
 $ErrorActionPreference = "Stop"
-Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 dotnet clean
-dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version
-dotnet test --configuration Release --no-build "Axoom.Extensions.Logging.UnitTests/Axoom.Extensions.Logging.UnitTests.csproj"
+dotnet msbuild -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version
 
-Pop-Location
+popd
